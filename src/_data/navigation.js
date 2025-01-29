68156150805f4316be4b0fc86a30e4dd77023132
Louis Maher <louismaher@gmail.com>
2025-01-29 15:46:53 -0500

    Spotify

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -5,6 +5,12 @@ module.exports = {
       text: 'Bandcamp',
       url: 'https://fredmahermusique.bandcamp.com/'
     },
+
+    {
+      text: 'Spotify',
+      url: 'https://open.spotify.com/intl-fr/artist/7tFUZvZEblOQAV82CC5voj'
+    },
+   
     {
       text: 'Youtube',
       url: 'https://www.youtube.com/@fredmaher1201/playlists'
